bd348a2425779af85fe0af6b5e729139a723772e
nk65111 <nk6511165@gmail.com>
2025-02-24 18:58:26 +0530

    commit"redischange"

--- backend/redis.js
+++ backend/redis.js
@@ -1,4 +1,20 @@
 var Redis = require('ioredis');
-var client = new Redis();
 
-module.exports=client;
+require('dotenv').config();
+
+var client = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+});
+
+client.on('connect', function() {
+    console.log('✅ Connected to Redis');
+});
+
+client.on('error', function(err) {
+    console.log('❌ Error: ' + err);
+});
+
+module.exports = client;
